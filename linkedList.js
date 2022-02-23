203e01136d8ddce1a8319d1aa97d4e8fc3c3126c
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2022-02-23 14:09:54 -0800

    linked list remove from tail

--- linkedList.js
+++ linkedList.js
@@ -42,18 +42,46 @@ class LinkedList {
 
     addToTailOpt(value) {   //O(n)
         const newNode = new Node(value);
+
+        if(!this.tail) {
+            this.head = newNode;
+            this.tail = this.head;
+            return;
+        }
+
         this.tail.next = newNode;
         this.tail = newNode;
     }
 
-    removeFromHead(){
-        let curr = this.head;
-        this.head = curr.next;
-        curr.next = null;
+    removeFromHead(){   //O(1)
+        if(this.head) {
+            let curr = this.head;
+            this.head = curr.next;
+            curr.next = null;
+        }
+
         this.printLL();
     }
 
-    
+    removeFromTail() {  //O(n)
+        let curr = this.head;
+
+        if(!curr) return;
+        if(!curr.next) {
+            this.head = null;
+            this.tail = null;
+            this.printLL();
+            return;
+        }
+
+        while(curr.next.next) {
+            curr = curr.next;
+        }
+
+        curr.next = null;
+        this.tail = curr;
+        this.printLL();
+    }
 
     printLL() { //O(1)
         let curr = this.head;
@@ -80,9 +108,14 @@ class LinkedList {
 const ll = new LinkedList();
 // ll.head = node1;
 ll.printLL();
+ll.removeFromHead();
+ll.removeFromTail();
 ll.addToHead(1);
+ll.printLL();
+ll.removeFromHead();
 ll.addToHead(0);
 ll.printLL();
+ll.removeFromTail();
 ll.addToTailOpt(2);
 ll.addToTailOpt(3);
 ll.printLL();
@@ -90,3 +123,4 @@ ll.addToHead(-1);
 ll.addToHead(-2);
 ll.printLL();
 ll.removeFromHead();
+ll.removeFromTail();
