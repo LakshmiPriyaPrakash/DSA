d5b3d87d5b6a12dc5f95a72bbc7f5f1022a04b08
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2022-02-24 13:13:58 -0800

    array methods 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,20 @@ const a6 = [1, 2, 3, 4, 5, 6, 7];
 
 // console.log(a6.find((num) => num > 5))
 // console.log(a6.findIndex((num) => num > 5))
+
+const a7 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+
+// console.log(a7.flat()); //depth defaults to 1
+// console.log(a7.flat(2));
+// console.log(a7.flat(Infinity));
+
+
+const a8 = [1, 2, 3, 4, 5];
+
+const arr = a8.splice(1, 3, -2, -3, -4);
+console.log(arr);
+console.log(a8);
+
+const arr2 = a8.splice(1, 3, "*");
+console.log(arr2);
+console.log(a8);
