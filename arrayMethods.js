7f51d4085763d049749418e912819a13361fb339
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2022-02-24 12:31:08 -0800

    array methods 1

---
+++ arrayMethods.js
@@ -0,0 +1,18 @@
+const a0 = [1, 2, 3];
+const a1 = [4, 5, 6];
+const a2 = [7, 8, 9];
+
+const a3 = a1.concat(a2, a0);
+
+// console.log(a3)
+
+const a4 = [2, 4, 6];
+const a5 = [2, 4, 5, 6];
+
+// console.log(a4.every((num) => num % 2 === 0))
+// console.log(a5.every((num) => num % 2 === 0))
+
+const a6 = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(a6.find((num) => num > 5))
+// console.log(a6.findIndex((num) => num > 5))
